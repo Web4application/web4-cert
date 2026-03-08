5a51993727f0852224fc1315bd5444a50a5ee894
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-08 14:25:27 +0100

    Update web4-cert-generator.sh

--- docker build/-t web4-cert-generator/web4-cert-generator.sh
+++ docker build/-t web4-cert-generator/web4-cert-generator.sh
@@ -5,3 +5,8 @@ docker run --rm \
   -e WEB4_SERVER_SANS="ci.web4.com,api.web4.com" \
   -v $(api)/web4_certs:/app/web4_certs \
   web4-cert-generator
+ls $(api)/web4_certs
+openssl x509 -in "$(api)/web4_certs/ci.web4.com.crt" -text -noout
+openssl verify -CAfile "$(api)/web4_certs/WEB4 Root CA.crt" "$(api)/web4_certs/ci.web4.com.crt"
+openssl s_server -cert "$(api)/web4_certs/ci.web4.com.crt" -key "$(api)/web4_certs/ci.web4.com.key" -accept 8443
+openssl s_client -connect localhost:8443 -CAfile "$(api)/web4_certs/WEB4 Root CA.crt"
